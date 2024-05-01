cd23e6c708cfc0c3ecd207702d34a64b2e457661
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-05-01 09:53:57 -0600

    Preparacion para el manejo de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CharacteresListModule } from './components/pages/characters/characteres-list/characteres-list.module';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) }, { path: 'not', loadChildren: () => import('./components/pages/notFound/not-found/not-found.module').then(m => m.NotFoundModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/characteres-list/characteres-list-routing.module').then(m => m.CharacteresListRoutingModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/characteres-list/characteres-list.module').then(m => m.CharacteresListModule) }, { path: 'character-details', loadChildren: () => import('./components/pages/characters/characteres-details/characteres-details.module').then(m => m.CharacteresDetailsModule) }, { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) }];
-
+//DEFINIR RUTAS
+const routes: Routes = [
+  { path:  '', redirectTo: 'character-list', pathMatch:'full'},
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, 
+  { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
+  { path: 'character-list', loadChildren: () => import('./components/pages/characters/characteres-list/characteres-list.module').then(m => m.CharacteresListModule) }, 
+  { path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/characteres-details/characteres-details.module').then(m => m.CharacteresDetailsModule) }, 
+  { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) },
+  { path: '**', loadChildren: () => import('./components/pages/notFound/not-found/not-found.module').then(m => m.NotFoundModule) }
+];
+//PATH ** POR SI NINGUNA RUTA SE CUMPLE
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
