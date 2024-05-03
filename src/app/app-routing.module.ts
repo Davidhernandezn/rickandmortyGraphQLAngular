ca1ecb5bce76f99b492e62299188ea54ec407aff
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-05-02 20:09:41 -0600

    Se agrego pagiana about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
   { path: 'character-list', loadChildren: () => import('./components/pages/characters/characteres-list/characteres-list.module').then(m => m.CharacteresListModule) }, 
   { path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/characteres-details/characteres-details.module').then(m => m.CharacteresDetailsModule) }, 
-  { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) },
+  { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) },
   { path: '**', loadChildren: () => import('./components/pages/notFound/not-found.module').then(m => m.NotFoundModule) }
 ];
 //PATH ** POR SI NINGUNA RUTA SE CUMPLE
