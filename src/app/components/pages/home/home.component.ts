980e81f9e405a3baeb29d5f95257a37a268e5599
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-05-04 16:27:36 -0600

    Se agrego favoritos y muestra de ellos en el home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '@app/shared/services/localStorage.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+charactersFav$ = this.localStorageSvc.charactersFav$;//IGUAL AL OBSERVABLE
+  //inyectamos service
+  constructor(private localStorageSvc: LocalStorageService) { }
 
   ngOnInit(): void {
   }
--- src/app/components/pages/home/home.module.ts
+++ src/app/components/pages/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CharacteresCardModule } from '../characters/characteres-card/characteres-card.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CharacteresCardModule,//IMPORTAR PARA USAR
   ]
 })
 export class HomeModule { }
