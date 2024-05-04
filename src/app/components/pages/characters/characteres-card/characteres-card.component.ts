8b1fcad3751c899296ca45ea89ccdd8e49c89036
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-05-03 21:58:39 -0600

    Diseño actulizado preparacion de boton agregar favoritos

--- src/app/components/pages/characters/characteres-card/characteres-card.component.ts
+++ src/app/components/pages/characters/characteres-card/characteres-card.component.ts
@@ -1,15 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Character } from '@app/shared/interfaces/data.interface';
 
 @Component({
   selector: 'app-characteres-card',
   templateUrl: './characteres-card.component.html',
-  styleUrls: ['./characteres-card.component.scss']
+  styleUrls: ['./characteres-card.component.scss'],
+  changeDetection:ChangeDetectionStrategy.OnPush //EL COMPONENTE SE RENDERIZE CUANDO LA PROPIEDAD CAMBIE
 })
 export class CharacteresCardComponent implements OnInit {
-@Input() character;
+@Input() character:Character;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  getIcon() : string{
+    return this.character.isFavorite ? 'heart-solid.svg' : 'heart.svg';
+  }
+  
+  toggleFavorite():void{
+    const isFavorite = this.character.isFavorite;
+    this.getIcon();
+    this.character.isFavorite = !isFavorite;//VALIDA PARA SER TRUE O FALSE
+    //validar que favorite este definido como propiedad 
   }
 
 }
--- src/app/components/pages/characters/characteres-card/characteres-card.module.ts
+++ src/app/components/pages/characters/characteres-card/characteres-card.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacteresCardComponent } from './characteres-card.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [CharacteresCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule//PARA ENVIAR A OTRA RUTA INDICADA
   ],
   exports:[CharacteresCardComponent] //CUALQUIER MODULO QUE USE ESTE MODULO PUEDE USAR ESTE COMPONENTES (CHARACTER CARD)
 })
